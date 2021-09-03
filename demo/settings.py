2081caca99cdc14d6c5e6ab5efd6c48dc1975f4e
juveriya-khatri <59953089+juveriya-khatri@users.noreply.github.com>
2021-09-03 14:34:35 +0530

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -136,8 +136,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL = '/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
