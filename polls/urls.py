409eba370ca92aea2f1ee5368de7cc61176ae72f
juveriya-khatri <59953089+juveriya-khatri@users.noreply.github.com>
2021-09-03 14:35:58 +0530

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -3,18 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.apiOverview, name="api-overview"),
+	path('students/', views.StudentList.as_view()),
 
-	path('student-list/', views.studentList, name="student-list"),
-
-	path('student-detail/<str:pk>/', views.studentDetail, name="student-detail"),
-
-	path('student-create/', views.studentCreate, name="student-create"),
-
-	path('student-update/<str:pk>/', views.studentUpdate, name="student-update"),
-
-	path('student-delete/<str:pk>/', views.studentDelete, name="student-delete"),
-
-	path('student/', views.listview, name='list'),
-
+	path('students/<int:pk>/', views.StudentDetail.as_view()),
 ]
