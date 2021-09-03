38b03789de4ca4c46c144250930d9f0ac992e5b6
juveriya-khatri <59953089+juveriya-khatri@users.noreply.github.com>
2021-09-03 14:33:24 +0530

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -2,9 +2,20 @@ from django.db import models
 
 
 class Student(models.Model):
+    def images(instance, filename):
+        return '/'.join([images, str(instance.name), filename])
     name = models.CharField(max_length=200)
     contact_number = models.CharField(max_length=200)
     email_id = models.EmailField()
+    image = models.ImageField(upload_to="images", null="True", blank="True")
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
     def __str__(self):
         return self.name
